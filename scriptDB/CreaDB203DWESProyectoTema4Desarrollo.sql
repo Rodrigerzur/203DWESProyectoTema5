b78312b590d6e30dad6d7b9c7af5f163d05f4ce4
Rodrigo <dragorodri@gmail.com>
2021-12-10 18:57:20 +0100

    10/12/2021 Solucionado problema con un nombre de un campo en la base de datos

--- scriptDB/CreaDB203DWESProyectoTema4Desarrollo.sql
+++ scriptDB/CreaDB203DWESProyectoTema4Desarrollo.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS T01_Usuario (
     T01_Password VARCHAR(255) NOT NULL,
     T01_DescUsuario VARCHAR(255) NOT NULL,
     T01_NumConexiones INT DEFAULT 0 NOT NULL,
-    T01_FechaHoraUltimaConexionAnterior DATETIME NULL ,
+    T01_FechaHoraUltimaConexion DATETIME NULL ,
     T01_Perfil enum('administrador', 'usuario') DEFAULT 'usuario', 
     T01_ImagenUsuario mediumblob
 ) ENGINE=INNODB;
